c52568553a33b17b84efa11955ac69a8e3997721
Jake <jake@fulmine.xyz>
2022-11-12 18:32:03 +0100

    Add title/head to missing pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,3 +13,13 @@
         </nav>
     </section>
 </main>
+
+<svelte:head>
+    <title>Fulmine | PluralKit</title>
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Fulmine | PluralKit" />
+    <meta property="og:description" content="A small website that lets you view some PluralKit data." />
+    <meta property="og:url" content="https://pk.fulmine.xyz" />
+    <meta property="og:image" content="/favicon.png" />
+    <meta name="theme-color" content="#ffffff">
+</svelte:head>
--- src/routes/m/+page.svelte
+++ src/routes/m/+page.svelte
@@ -16,3 +16,13 @@
         <span>(<a href="/">Back to home</a>)</span>
     </section>
 </main>
+
+<svelte:head>
+    <title>Fulmine | PluralKit</title>
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Fulmine | PluralKit" />
+    <meta property="og:description" content="A small website that lets you view some PluralKit data." />
+    <meta property="og:url" content="https://pk.fulmine.xyz" />
+    <meta property="og:image" content="/favicon.png" />
+    <meta name="theme-color" content="#ffffff">
+</svelte:head>
