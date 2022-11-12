dea40b6f0fd035aa278865e9737b112b697d5bb7
Jake <jake@fulmine.xyz>
2022-11-12 20:37:00 +0100

    tweak system fronting strings a little

--- src/lib/functions/strings/system.ts
+++ src/lib/functions/strings/system.ts
@@ -3,13 +3,13 @@ import type { System } from '$lib/types'
 // Builds the page title for the current fronters page
 export function buildFrontPageTitle(system: System) {
     if (system.name) return `Currently fronting in ${system.name}!`
-    else return `Currently fronting in [${system.id}]...`;
+    else return `System with id ${system.id}'s front...`;
 }
 
 // Builds the page title for the fronters if there's no fronters
 export function buildSwitchOutTitle(system: System) {
     if (system.name) return `${system.name} are currently switched out!`
-    else return `System [${system.id}] are currently switched out.`;
+    else return `System with id ${system.id} are currently switched out.`;
 }
 
 // Builds the opengraph embed title for the current fronters page
