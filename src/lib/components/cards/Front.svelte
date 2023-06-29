4571c8f907c536329dd8faa84e6cfd8d9047ad02
Jake Fulmine <jake@fulmine.xyz>
2023-06-29 12:48:14 +0200

    use regular avatars again

--- src/lib/components/cards/Front.svelte
+++ src/lib/components/cards/Front.svelte
@@ -13,7 +13,7 @@
 <a class="front link" href={`${system? `/s/${member.id}` : `/m/${member.id}`}${linkParams && linkParams.length > 0 ? `?${linkParams.join("&")}` : ""}`} >
     <div class="card front" style={getColor(member) ? `border-bottom: 4px solid ${getColor(member)};` : ""}>
         {#if getAvatar(member)}
-        <img class="avatar" style="width: 100%; aspect-ratio: 1 / 1;" src={getAvatar(member, true)} alt={`${name}'s avatar`}>
+        <img class="avatar" style="width: 100%; aspect-ratio: 1 / 1;" src={getAvatar(member)} alt={`${name}'s avatar`}>
         {/if}
         <div class="desc">
             <h3 class="name">{@html convertToHTML(name)}</h3>
