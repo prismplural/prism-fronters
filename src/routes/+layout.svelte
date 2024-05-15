d0b7113a6be21fb1f9934460a6d1b44005142cad
Jake Fulmine <jake@fulmine.xyz>
2024-05-15 23:10:28 +0200

    blargh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-  import { browser } from "$app/environment"
   import NProgress from "nprogress"
   import { setBodyTheme } from "$lib/functions/misc"
   import { navigating } from "$app/stores"
-  import GlobalStyle from "$lib/styles/global.scss"
-  import GitHubDark from "$lib/styles/github-dark.scss"
-  import GitHubLight from "$lib/styles/github.scss"
+  import "$lib/styles/global.scss"
+  import "$lib/styles/github-dark.scss"
+  import "$lib/styles/github.scss"
 
   // NProgress css
   import "nprogress/nprogress.css"
