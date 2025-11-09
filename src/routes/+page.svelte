72f63ee94e79e9aa6c66357d9e799c5addd849fd
Fulmine <jake@pals.gay>
2025-11-09 06:39:34 +0100

    fix: source code URL

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
       <a class="button" href="/s">System</a>
       <a class="button" href="/f">Front</a>
       <a class="button" href="/m">Member</a>
-      <a class="button" href="https://codeberg.org/fulmine/pk-fulmine">Source</a>
+      <a class="button" href="https://codeberg.org/fulmine/pluralkit-xyz">Source</a>
       <button class="button" on:click={() => changeTheme(theme)}>Theme</button>
     </nav>
   </section>
