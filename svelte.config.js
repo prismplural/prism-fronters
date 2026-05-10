5d7165454606cdc2e9e19bb79b8eae77f8c4ee89
skylar taylor-barrick <hi@skylar.cc>
2026-05-10 17:10:26 -0600

    fix: deploy SvelteKit on Netlify
    Use the Netlify adapter so SSR routes are served by a Netlify Function.
    
    Add Netlify build config and ignore generated local Netlify output.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node"
+import adapter from "@sveltejs/adapter-netlify"
 import { sveltePreprocess } from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,7 @@ const config = {
   preprocess: sveltePreprocess(),
 
   kit: {
-    adapter: adapter({
-      out: "build",
-    }),
+    adapter: adapter(),
   },
 }
 
